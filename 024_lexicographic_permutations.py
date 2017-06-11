d803ba782d8c96eeb7e3f27983583d1d39dae952
Fred Gurkov <Fred.Gurkov@macquarie.com>
2017-06-11 15:53:33 +0800

    024: Re-implemented using permutation generator

--- 024_lexicographic_permutations.py
+++ 024_lexicographic_permutations.py
@@ -4,18 +4,24 @@ Created on 11 Jun 2017
 @author: fgurkov1
 '''
 from typing import List
-from tkinter.constants import CURRENT
 from pprint import pprint
+from _collections import defaultdict
+
+class State(object):
+    
+    def __init__(self):
+        self.elements = []
+        self.i = 0
 
 class LexPerm(object):
     
     def __init__(self, elements:List):
-        self.elements = elements
+        self.elements = list(elements)
         self.elements.sort()
-        self.current = self.elements
-        self.stacks = []
-        for i in len(self.elements):
-            self.stacks[i] = self.elements[i:]
+        self.current = list(elements)
+        self.states = defaultdict(lambda:State())
+        self.reset_state(0, self.elements)
+        self.has_more = True
 
     def __iter__(self):
         return self
@@ -24,24 +30,33 @@ class LexPerm(object):
         return self.next()
     
     def next(self):
-        result = self.current
-        self.permutate()
-        return result
+        if not self.has_more:
+            raise StopIteration()
+        else:
+            self.has_more = not self.permutate(0)
+            return self.current
 
-perm_count = 0
+    def permutate(self, position:int):
+        state = self.states[position]
+        if len(state.elements) == 0:
+            return True
+        self.current[position] = state.elements[state.i]
+        if self.permutate(position + 1):
+            state.i += 1
+            if state.i == len(state.elements):
+                return True
+            next_state_elements = state.elements[0:state.i] + state.elements[state.i + 1:]
+            self.reset_state(position + 1, next_state_elements)
+        return False
 
-def permutate(elements:List, previous:List, limit:int):
-    global perm_count
-    if perm_count >= limit:
-        return
-    if len(elements) == 0:
-        perm_count += 1
-        if perm_count == limit:
-            pprint(previous)
-            return
-    else:
-        for i in range(len(elements)):
-            permutate(elements[0:i] + elements[i+1:], previous + [elements[i]], limit)
-        
+    def reset_state(self, position, elements):
+        for i in range(len(elements) + 1):
+            next_state = self.states[position + i]
+            next_state.i = 0
+            next_state.elements = elements[i:]
+
 if __name__ == '__main__':
-    permutate([i for i in range(10)], [], 1000000)
+    permutator = LexPerm([d for d in range(10)])
+    for i in range(1000000):
+        permutation = next(permutator)
+    pprint("".join([str(element) for element in permutation]))
